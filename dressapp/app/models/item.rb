8d9c16b0db091c1b7cf7499a11c1950d040c0355
sofiamatulis <sosomatulis@hotmail.com>
2016-09-26 17:43:04 -0400

    fixed image problem for item

--- dressapp/app/models/item.rb
+++ dressapp/app/models/item.rb
@@ -1,5 +1,12 @@
 class Item < ApplicationRecord
+
+  has_attached_file :image, default_url: 'http://lorempixel.com/g/200/200/'
+
+
   belongs_to :category
   belongs_to :wardrobe, optional: true
   belongs_to :suitcase, optional: true
+
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
 end
--- dressapp/db/schema.rb
+++ dressapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926203404) do
+ActiveRecord::Schema.define(version: 20160926214148) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "category"
@@ -23,8 +23,12 @@ ActiveRecord::Schema.define(version: 20160926203404) do
     t.text     "description"
     t.string   "image"
     t.integer  "category_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
     t.index ["category_id"], name: "index_items_on_category_id"
   end
 
