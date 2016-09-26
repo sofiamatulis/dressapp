f73a3927d4635e0b19463d1edc32012e23569f9c
sofiamatulis <sosomatulis@hotmail.com>
2016-09-26 14:35:20 -0400

    created routes

--- dressapp/config/routes.rb
+++ dressapp/config/routes.rb
@@ -8,10 +8,13 @@ Rails.application.routes.draw do
 
 
   resources :sessions,    only: [:new, :create, :destroy]
+
+  resources :users
+  resources :wardrobes
+  resources :items
+  resources :suitcases
   
-  resources :users do
-    resources :wardrobes
-  end
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
