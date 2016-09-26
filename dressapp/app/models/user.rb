c9b25b2dd8c6c5c45d6bb7f8a1b18c6447bf0e65
sofiamatulis <sosomatulis@hotmail.com>
2016-09-26 16:31:51 -0400

    changed user model

--- dressapp/app/models/user.rb
+++ dressapp/app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_attached_file :image, default_url: 'http://lorempixel.com/g/200/200/'
+
+
   has_many :wardrobes
   has_many :suitcases
 
   validates :name, :password_digest, presence: true
   validates :email, uniqueness: true
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
 
 end
