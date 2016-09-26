669d3f44374a9388f358f6c3073029b4387fe7c8
sofiamatulis <sosomatulis@hotmail.com>
2016-09-26 13:54:23 -0400

    created methods for controllers in user

--- dressapp/app/controllers/users_controller.rb
+++ dressapp/app/controllers/users_controller.rb
@@ -1,13 +1,41 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to wardrobes_url, notice: "Account created"
+    else
+      render "new"
+    end
   end
 
   def new
+    @user = User.new
   end
 
   def edit
+    @user= User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_edit_params)
+      redirect_to user_url(@user)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation, :email, :image)
+  end
+
+  def user_edit_params
+    params.require(:user).permit(:name, :image)
+  end 
+
 end
--- dressapp/app/models/user.rb
+++ dressapp/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+
   has_many :wardrobes
   has_many :suitcases
+
+  validates :name, :password_digest, presence: true
+  validates :email, uniqueness: true
+
 end
