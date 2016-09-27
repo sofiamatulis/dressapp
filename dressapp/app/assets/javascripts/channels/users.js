16eae0bedcacc0fda4e036108440984e0e1130e7
sofiamatulis <sosomatulis@hotmail.com>
2016-09-27 17:46:58 -0400

    prevent default when clicking ajax

--- dressapp/app/assets/javascripts/channels/users.js
+++ dressapp/app/assets/javascripts/channels/users.js
@@ -1,10 +1,20 @@
 $(function(){
 
-$('.wardrobebutton').on('click',function (event){
+$('.wardrobebutton').on('click',function(event){
 
   //prevents the default from happening which is going to a new page
 
   event.preventDefault();
+
+  $.ajax({
+    url:'/wardrobes/new',
+    method: 'POST',
+    data:{},
+    dataType: 'html'
+
+  }).done(function(data){
+    console.log(data);
+  });
 
 });
 
