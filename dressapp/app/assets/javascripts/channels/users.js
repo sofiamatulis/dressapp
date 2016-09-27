13de4873b1d188f962459701d4a68f7924d59785
sofiamatulis <sosomatulis@hotmail.com>
2016-09-27 17:06:43 -0400

    prevented default to go to a new page

--- dressapp/app/assets/javascripts/channels/users.js
+++ dressapp/app/assets/javascripts/channels/users.js
@@ -1,6 +1,12 @@
 $(function(){
 
+$('.wardrobebutton').on('click',function (event){
 
+  //prevents the default from happening which is going to a new page
+
+  event.preventDefault();
+
+});
 
 
 });
