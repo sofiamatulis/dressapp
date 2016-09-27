54669f1fd809f627a5ef63ee70a6c77c98827adb
Ryan <ryan.y.chung@gmail.com>
2016-09-27 14:52:47 -0400

    moving branch changes to master repo

--- dressapp/app/controllers/suitcases_controller.rb
+++ dressapp/app/controllers/suitcases_controller.rb
@@ -1,19 +1,48 @@
 class SuitcasesController < ApplicationController
   def index
+    @suitcases = Suitcase.all
   end
 
   def show
-  end
-
-  def edit
+    @suitcase = Suitcase.find(params[:id])
   end
 
   def new
+    @suitcase = Suitcase.new
   end
 
   def create
+    @suitcase = Suitcase.new(suitcase_params)
+
+    if @suitcase.save
+      redirect_to suitcases_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @suitcase = Suitcase.find(params[:id])
+  end
+
+  def update
+    @suitcase = Suitcase.find(params[:id])
+
+    if @suitcase.update_attributes(suitcase_params)
+      redirect_to suitcase_url(@suitcase)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @suitcase = Suitcase.find(params[:id])
+    @suitcase.destroy
+    redirect_to suitcases_url
+  end
+
+  private
+  def suitcase_params
+    params.require(:suitcase).permit(:name, :description, :duration, :destination)
   end
 end
