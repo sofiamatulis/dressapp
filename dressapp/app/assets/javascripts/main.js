6b63b63b141333087421e0381b65a3dc7cf728c4
sofiamatulis <sosomatulis@hotmail.com>
2016-09-28 11:50:29 -0400

    got javascript to work

--- dressapp/app/assets/javascripts/main.js
+++ dressapp/app/assets/javascripts/main.js
@@ -1,7 +1,6 @@
 $(function(){
 
 $('.wardrobebutton').click( function(){ addWardrobe() });
-$('.modal').click( function(){close()});
 $('.close').click( function(){close()});
   // $('.submit').click( function(event) { error(event) });
   // $('input').click( function(event){notError(event)});
