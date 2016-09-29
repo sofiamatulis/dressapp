e401131412242cb0d34ab6a75c71845e8c5de016
Edward Yu <3dward.yu@gmail.com>
2016-09-28 23:38:51 -0400

    change respond_to for wardrobe#show controller

--- dressapp/app/controllers/wardrobes_controller.rb
+++ dressapp/app/controllers/wardrobes_controller.rb
@@ -36,7 +36,7 @@ class WardrobesController < ApplicationController
    @item = Item.new(:wardrobe_id => params[:id])
    respond_to do |format|
      format.html
-     format.json { render json: {items: @items, users: @users,category: @category, wardrobe: @wardrobes,suitcase: @suitcases }}
+     format.json { render json: {item: @item.to_json, users: @users,category: @category, wardrobe: @wardrobe.to_json, suitcase: @suitcases }}
    end
   end
 
