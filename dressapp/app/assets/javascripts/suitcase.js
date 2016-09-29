ce47d24ca5cb4c00d1682c21b67fba406b44fe1e
Natalie Dodd <nataliedodd89@gmail.com>
2016-09-28 21:57:26 -0400

    added close nav functionality on suitcase show

--- dressapp/app/assets/javascripts/suitcase.js
+++ dressapp/app/assets/javascripts/suitcase.js
@@ -5,4 +5,9 @@ $(function(){
     document.getElementById("suitcase-main").style.marginLeft = "400px";
   });
 
+  $("#close-nav").on('click', function() {
+    document.getElementById("suitcase-side-nav").style.width = "0";
+    document.getElementById("suitcase-main").style.marginLeft= "0";
+  });
+
 });
