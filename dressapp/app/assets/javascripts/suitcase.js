608a48b3d3c0bc7feab880b6e0ed2df970948628
Natalie Dodd <nataliedodd89@gmail.com>
2016-09-29 18:47:27 -0400

    working on carousel

--- dressapp/app/assets/javascripts/suitcase.js
+++ dressapp/app/assets/javascripts/suitcase.js
@@ -1,13 +1,17 @@
 $(function(){
 
   $("#open-nav").on('click', function() {
-    document.getElementById("suitcase-side-nav").style.width = "50%";
-    document.getElementById("suitcase-main").style.marginLeft = "50%";
+    document.getElementById("suitcase-side-nav").style.width = "100%";
+    document.getElementById("suitcase-main").style.marginLeft = "100%";
   });
 
   $("#close-nav").on('click', function() {
     document.getElementById("suitcase-side-nav").style.width = "0";
     document.getElementById("suitcase-main").style.marginLeft= "0";
+  });
+
+  $('.clothes-images').on('click', function() {
+    document.getElementsByClass('.clothes-images').carousel()
   });
 
 });
