02d2fbc17c55b2b809132daa63871fd689047fbf
Natalie Dodd <nataliedodd89@gmail.com>
2016-09-28 21:49:00 -0400

    fixed suitcase side nav to transition more smoothly

--- dressapp/app/assets/javascripts/suitcase.js
+++ dressapp/app/assets/javascripts/suitcase.js
@@ -2,7 +2,7 @@ $(function(){
 
   $("#open-nav").on('click', function() {
     document.getElementById("suitcase-side-nav").style.width = "400px";
-    document.getElementById("suitcase-main").style.marginLeft = "250px";
+    document.getElementById("suitcase-main").style.marginLeft = "400px";
   });
 
 });
