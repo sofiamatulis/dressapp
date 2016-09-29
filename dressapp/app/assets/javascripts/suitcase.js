ce5a7504083770224c09463e4458094d1c3585e1
Natalie Dodd <nataliedodd89@gmail.com>
2016-09-28 21:37:22 -0400

    created side nav in suitcase show

---
+++ dressapp/app/assets/javascripts/suitcase.js
@@ -0,0 +1,8 @@
+$(function(){
+
+  $("#open-nav").on('click', function() {
+    document.getElementById("suitcase-side-nav").style.width = "400px";
+    document.getElementById("suitcase-main").style.marginLeft = "250px";
+  });
+
+});
