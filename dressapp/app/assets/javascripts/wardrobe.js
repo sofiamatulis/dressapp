35db02c5c8f49ff041a982ded5bfb2d4c3507f04
Edward Yu <3dward.yu@gmail.com>
2016-09-29 20:16:29 -0400

    trying to fix submit disabled

--- dressapp/app/assets/javascripts/wardrobe.js
+++ dressapp/app/assets/javascripts/wardrobe.js
@@ -6,6 +6,7 @@ $(function(){
     $('.modal-item').fadeIn('fast');
 
     $('.modal-form').on('submit', function(event){
+      $('.item-create').prop('disabled', false);
       event.preventDefault();
       $('.modal-item').fadeOut('slow');
 
@@ -15,12 +16,14 @@ $(function(){
         data: $(this).serialize(),
         dataType: 'json'
       }).done(function(response){
+        $(".modal-form")[0].reset();
         var name = $('<li class="item-details">').append(response.name);
         var description = $('<li class="item-details">').append(response.description);
         var image = $('<img>').attr("src", response.image);
         var imagelink = $('<a href="http://localhost:3000/items/' + response.id + '">').append(image);
         var imageappend = $('<li class="item-details">').append(imagelink);
         $('.wardrobe-item').append(name, description, imageappend);
+
 
       });
     });
