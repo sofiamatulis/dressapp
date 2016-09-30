5c43c2d3dd3c48dd9d0ff465e57ee7b996e6ae11
Natalie Dodd <nataliedodd89@gmail.com>
2016-09-29 23:33:10 -0400

    finally got slider working with slick, prev and next buttons need styling

--- dressapp/app/assets/javascripts/suitcase.js
+++ dressapp/app/assets/javascripts/suitcase.js
@@ -1,26 +1,49 @@
 $(function(){
 
   $("#open-nav").on('click', function() {
-    document.getElementById("suitcase-side-nav").style.width = "100%";
-    document.getElementById("suitcase-main").style.marginLeft = "100%";
+    document.getElementById("suitcase-side-nav").style.width = "40%";
+    document.getElementById("suitcase-main").style.marginLeft = "40%";
   });
 
   $("#close-nav").on('click', function() {
     document.getElementById("suitcase-side-nav").style.width = "0";
     document.getElementById("suitcase-main").style.marginLeft= "0";
   });
+  //
+  // $(".arrow-right").on('click', function() {
+  //     if ($('.clothes-item').hasClass('active')) {
+  //       $('.clothes-item').removeClass('active').addClass('inactive');
+  //       $('.clothes-item').next().removeClass('inactive').addClass('active');
+  //     }
+  //   });
+  //
+  // $(".arrow-left").on('click', function() {
+  //     if ($('.clothes-item').hasClass('active')) {
+  //       $('.clothes-item').removeClass('active').addClass('inactive');
+  //       $('.clothes-item').prev().removeClass('inactive').addClass('active');
+  //     }
+  //   });
 
-  $(".arrow-right").on('click', function() {
-      if ($('.clothes-item').hasClass('active')) {
-        $('.clothes-item').removeClass('active').addClass('inactive');
-        $('.clothes-item').next().removeClass('inactive').addClass('active');
-      }
+
+    $('.tops-container').slick( {
+      centerMode: true,
+      // prevArrow: $('.arrow-left'),
+      // nextArrow: $('.arrow-right'),
     });
 
-  $(".arrow-left").on('click', function() {
-      if ($('.clothes-item').hasClass('active')) {
-        $('.clothes-item').removeClass('active').addClass('inactive');
-        $('.clothes-item').prev().removeClass('inactive').addClass('active');
-      }
-    }); 
+    $('.bottoms-container').slick( {
+      // prevArrow: '<button class="arrow-left">PREV</button>',
+      // nextArrow: '<button class="arrow-right">NEXT</button>',
+    });
+
+
+
+
+
+    var slideWidth = $('.clothes-item span').width();
+    //
+    // function moveLeft() {
+    //     $('.clothes-item img').animate({
+    //         left: + slideWidth
+    //     }
 });
