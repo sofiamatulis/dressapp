4ec2e2d81497bdd39f8bfe9e1851cb47e2693c29
sofiamatulis <sosomatulis@hotmail.com>
2016-10-01 11:05:33 -0400

    created drop branch

--- dressapp/app/assets/javascripts/main.js
+++ dressapp/app/assets/javascripts/main.js
@@ -61,38 +61,5 @@ $(function(){
       });
 
 
-//       $('#imageUploadForm').on('submit',(function(event) {
-//       event.preventDefault();
-//       var formData = new FormData(this);
-//
-//       $.ajax({
-//           type:'POST',
-//           url: $(this).attr('action'),
-//           data:formData,
-//           cache:false,
-//           contentType: false,
-//           processData: false,
-//           success:function(data){
-//               console.log("success");
-//               console.log(data);
-//           },
-//           error: function(data){
-//               console.log("error");
-//               console.log(data);
-//           }
-//       });
-//   }));
-//
-//   $("#ImageBrowse").on("change", function() {
-//       $("#imageUploadForm").submit();
-//   });
-// });
-
-
-
-
-
-
-
 
 });
