cd8461135796a47d864560ac3e95fa85258e552a
sofiamatulis <sosomatulis@hotmail.com>
2016-10-03 16:11:15 -0400

    redirects to main page when you log out

--- dressapp/app/controllers/sessions_controller.rb
+++ dressapp/app/controllers/sessions_controller.rb
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to wardrobes_url, notice: "Logged out"
+    redirect_to root_path, notice: "Logged out"
   end
 end
