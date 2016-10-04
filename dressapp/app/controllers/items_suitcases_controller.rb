08136f06bfbe3e0b9ee10984201f8c93411dd7f4
sofiamatulis <sosomatulis@hotmail.com>
2016-10-04 18:09:07 -0400

    fixed back end to add item to suitcase

--- dressapp/app/controllers/items_suitcases_controller.rb
+++ dressapp/app/controllers/items_suitcases_controller.rb
@@ -4,22 +4,18 @@ class ItemsSuitcasesController < ApplicationController
 
 
   def create
-    @item = Item.find(params[:item])
-    # @item_id = Item.find(params[:suitcase][:item_ids])
-    @suitcases = current_user.suitcases
-    @suitcase = Suitcase.find(params[:suitcase])
-    # redirect_to item_path(@item)
-
-# adding an item at a time to the suitcase:
-
-  end
 
 
-  def update
+    # adding an item at a time to the suitcase from the suitcase page :
     @item = Item.find(params[:item_id])
-    @suitcase = Suitcase.find(params[:id])
+    @suitcase = Suitcase.find(params[:suitcase_id])
     @suitcase.items << @item
+
+
+
   end
+
+
 
 
 
