476c811890b121784239e196d37823fd75ad9fd1
Natalie Dodd <nataliedodd89@gmail.com>
2016-10-05 12:34:31 -0400

    uncommented section of items_suitcases controller so item save works

--- dressapp/app/controllers/items_suitcases_controller.rb
+++ dressapp/app/controllers/items_suitcases_controller.rb
@@ -2,19 +2,16 @@ class ItemsSuitcasesController < ApplicationController
 
   before_action :ensure_logged_in
 
-#
   def create
-# <<<<<<< HEAD
-#
-#
-#     # adding an item at a time to the suitcase from the suitcase page :
-#     @item = Item.find(params[:item_id])
-#     @suitcase = Suitcase.find(params[:suitcase_id])
-#     @suitcase.items << @item
-#
-#
-#
-# =======
+
+
+
+    # adding an item at a time to the suitcase from the suitcase page :
+    @item = Item.find(params[:item_id])
+    @suitcase = Suitcase.find(params[:suitcase_id])
+    @suitcase.items << @item
+
+
     items_ary = []
     #
     # params['items'].each do |item|
