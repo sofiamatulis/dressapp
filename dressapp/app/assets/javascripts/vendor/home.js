15ecf0bdd23429b5ac9c613a7514be502daff247
sofiamatulis <sosomatulis@hotmail.com>
2016-10-06 12:29:32 -0400

    created carousel for main page

---
+++ dressapp/app/assets/javascripts/vendor/home.js
@@ -0,0 +1,10 @@
+$( document ).on('ready turbolinks:load', function() {
+
+
+  $('.center').slick( {
+    prevArrow: '<span class="arrow-left"><</span>',
+    nextArrow: '<span class="arrow-right">></span>',
+  });
+
+
+});
