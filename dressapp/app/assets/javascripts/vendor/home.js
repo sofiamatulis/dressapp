83ea25a2de022876a2c2ea156970ea94c838872b
sofiamatulis <sosomatulis@hotmail.com>
2016-10-06 13:31:38 -0400

    edited login sign up page

--- dressapp/app/assets/javascripts/vendor/home.js
+++ dressapp/app/assets/javascripts/vendor/home.js
@@ -4,6 +4,10 @@ $( document ).on('ready turbolinks:load', function() {
   $('.center').slick( {
     prevArrow: '<span class="arrow-left"><</span>',
     nextArrow: '<span class="arrow-right">></span>',
+    autoplay: true,
+    autoplaySpeed: 1600,
+    speed: 1000,
+    fade: true,
   });
 
 
