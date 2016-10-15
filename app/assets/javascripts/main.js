0804117cef688e6adc6a6b14246e3dbd3cdefe06
sofiamatulis <sosomatulis@hotmail.com>
2016-10-15 15:08:10 -0400

    added comments to js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,7 @@
 $( document ).on('turbolinks:load', function() {
 
+  //creating a new wardrobe! when clicking on the button new wardrobe and appending it to the list
+
     $('#new_wardrobe').on('submit',function(event){
       event.preventDefault();
       $.ajax({
@@ -12,6 +14,8 @@ $( document ).on('turbolinks:load', function() {
         var link = $('<a class="link-text" href="http://localhost:3000/wardrobes/' + wardrobe.id +  '" >' + wardrobe.name + '</a>');
         var name = $ ('.allwardrobe').append("<li class='mywardrobe'>").append(link);
 
+        //styling the result
+
             if($('.mywardrobe').length != 0) {
                 $('.link-text').css('color', 'black');
                 $('.link-text').css('font-size', '30px');
@@ -19,9 +23,13 @@ $( document ).on('turbolinks:load', function() {
 
         }
 
+        //make sure you can create new wardrobes consicutively and form is empty again
+
         $( "#create-wardrobe").prop( "disabled", false );
         $("#new_wardrobe")[0].reset();
       });
+
+      //same for suitcase
 
       $('#new_suitcase').on('submit',function(event){
         event.preventDefault();
