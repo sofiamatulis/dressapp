f689158365bd1d45bd27b63d5d36ad49cf960d40
Natalie Dodd <nataliedodd89@gmail.com>
2016-10-15 18:27:22 -0400

    appended wardrobe image to page through ajax when wardrobe added

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,7 +9,7 @@ $( document ).on('turbolinks:load', function() {
         dataType: "json",
         data:  $('#new_wardrobe').serialize()
       }).done(function(wardrobe){
-        var link = $('<a class="link-text" href="http://localhost:3000/wardrobes/' + wardrobe.id +  '" >' + wardrobe.name + '</a>');
+        var link = $('<a class="name-wardrobe" href="http://localhost:3000/wardrobes/' + wardrobe.id +  '" >' + '<img src="/assets/mainwardrobe.png">' + '</a>');
         var name = $ ('.allwardrobe').append("<li class='mywardrobe'>").append(link);
 
             if($('.mywardrobe').length != 0) {
--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -14,6 +14,7 @@ $( document ).on('turbolinks:load', function() {
       });
       $('#new_wardrobe').on('submit', function(){
         $('.modal-wardrobe').fadeOut();
+        $('#allwardrobe').append('<img src="/clothes-wardrobe.png" height="100px" width="auto">');
 
       });
 
