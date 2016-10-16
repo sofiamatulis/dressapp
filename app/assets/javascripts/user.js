6ca28f73267f4724262ff81f9550a99c6c0e85d8
sofiamatulis <sosomatulis@hotmail.com>
2016-10-15 23:45:34 -0400

    hover

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -148,33 +148,33 @@ $( document ).on('turbolinks:load', function() {
 
 
 
-  // $('.name-wardrobe').hover(function(){
+  $('.name-wardrobe').hover(function(){
 
 // created variable mybox and whatever im hovering on (this), calling the parent my wardrobe and finding the class my box thats inside of this parent too
-    // var mybox = $(this).parent("li.mywardrobe").find('.mybox');
-    // var mywardrobe = $(this).attr("href");
-    // console.log(mywardrobe);
+    var mybox = $(this).parent("li.mywardrobe").find('.mybox');
+    var mywardrobe = $(this).attr("href");
+    console.log(mywardrobe);
 
     // console.log('start');
     //ajax get request to just show the thumbnail
 
-//     $.ajax({
-//       url: mywardrobe + '/thumbnail',
-//       method:'GET',
-//       dataType: "html",
-//
-//       //show the my box variable
-//       }).done(function(wardrobe){
-//         // console.log('done');
-//          mybox.html(wardrobe);
-//          mybox.show();
-//         // console.log(wardrobe);
-//       });
-// // hide when not hovering!
-//     }, function() {
-//     var mybox = $(this).parent("li.mywardrobe").find('.mybox');
-//       mybox.hide();
-//
-//     });
+    $.ajax({
+      url: mywardrobe + '/thumbnail',
+      method:'GET',
+      dataType: "html",
+
+      //show the my box variable
+      }).done(function(wardrobe){
+        // console.log('done');
+         mybox.html(wardrobe);
+         mybox.show();
+        // console.log(wardrobe);
+      });
+// hide when not hovering!
+    }, function() {
+    var mybox = $(this).parent("li.mywardrobe").find('.mybox');
+      mybox.hide();
+
+    });
 
   });
