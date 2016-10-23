10c1e880762975606837c49f1d1930c57badb249
Natalie Dodd <nataliedodd89@gmail.com>
2016-10-23 13:10:11 -0400

    ajax request set up for photo places request, but error message occurs without CORS

--- app/assets/javascripts/cityphoto.js
+++ app/assets/javascripts/cityphoto.js
@@ -12,11 +12,22 @@ $( document ).on('turbolinks:load', function() {
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode( { 'address': suitcase.destination }, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
-        alert("location : " + results[0].geometry.location.lat() + " " +results[0].geometry.location.lng());
+        var coordinates = results[0].geometry.location.lat() + ", " + results[0].geometry.location.lng();
+        console.log(coordinates);
+        // after getting the lat and long, use this in the ajax request to google to receive a photo reference
+          $.ajax({
+            url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + coordinates + '&radius=5000&type=landscape&keyword=tourism&key=AIzaSyAEmJ_uxarY1GFaiWbHvyX-AEkOK90rdyU',
+            method: 'GET',
+            data:{},
+            dataType: 'json'
+          }).done(function(response) {
+            console.log(response);
+          });
       } else {
-        alert("Something got wrong " + status);
-      }
+        alert("Unable to retrieve destination" + status);
+      }  
     });
+
   });
 
 });
